2aa7eefbdfd5acc0fc495a94e790d12b1595f81d
Sahil Mehta <mehtasahil881@berkeley.edu>
2021-07-11 19:47:41 -0700

    timer implementation (still in progress)

--- coloring/views.py
+++ coloring/views.py
@@ -5,7 +5,7 @@ def index(request):
     if request.method == 'POST':
         form = request.POST.dict()
         payload = {'tens_min': int(form['tens_min']), 'ones_min': int(form['ones_min']), \
-            'ones_sec': int(form['ones_sec']), 'ones_sec': int(form['ones_sec'])}
+            'tens_sec': int(form['tens_sec']), 'ones_sec': int(form['ones_sec'])}
         return render(request, 'coloring/new_interaction.html', payload)
     return render(request, 'coloring/new_interaction.html')
 
@@ -13,12 +13,6 @@ def demo(request):
     return render(request, 'coloring/demo.html')
 
 def start(request):
-    tens_min, ones_min, tens_sec, ones_sec = 0, 0, 0, 0
-    if request.method == 'POST':
-        form = request.POST.dict()
-        payload = {'tens_min': int(form['tens_min']), 'ones_min': int(form['ones_min']), \
-            'ones_sec': int(form['ones_sec']), 'ones_sec': int(form['ones_sec'])}
-        return render(request, 'coloring/start.html')
     return render(request, 'coloring/start.html')
 
 def timer(request):
