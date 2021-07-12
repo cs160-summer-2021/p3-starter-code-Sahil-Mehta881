da6f6c75122fbe9117c930d26853746a5818e082
Sahil Mehta <mehtasahil881@berkeley.edu>
2021-07-11 19:25:12 -0700

    backend timer

--- coloring/views.py
+++ coloring/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
+    tens_min, ones_min, tens_sec, ones_sec = 0, 0, 0, 0
+    if request.method == 'POST':
+        form = request.POST.dict()
+        payload = {'tens_min': int(form['tens_min']), 'ones_min': int(form['ones_min']), \
+            'ones_sec': int(form['ones_sec']), 'ones_sec': int(form['ones_sec'])}
+        return render(request, 'coloring/new_interaction.html', payload)
     return render(request, 'coloring/new_interaction.html')
 
 def demo(request):
     return render(request, 'coloring/demo.html')
 
 def start(request):
+    tens_min, ones_min, tens_sec, ones_sec = 0, 0, 0, 0
+    if request.method == 'POST':
+        form = request.POST.dict()
+        payload = {'tens_min': int(form['tens_min']), 'ones_min': int(form['ones_min']), \
+            'ones_sec': int(form['ones_sec']), 'ones_sec': int(form['ones_sec'])}
+        return render(request, 'coloring/start.html')
     return render(request, 'coloring/start.html')
 
 def timer(request):
