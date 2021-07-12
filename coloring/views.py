859e4273fa9048ec900a708d2e57b0664701f66e
Sahil Mehta <mehtasahil881@berkeley.edu>
2021-07-11 18:09:10 -0700

    set up files

--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'coloring/index.html')
+    return render(request, 'coloring/new_interaction.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def start(request):
+    return render(request, 'coloring/start.html')
+
+def timer(request):
+    return render(request, 'coloring/timer.html')
